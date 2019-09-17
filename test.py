c809d6b112a1b67d4ad9652dc035ebeeb9faf1c7
hejiay <hejiayuan0822@163.com>
2019-09-17 17:03:26 +0800

    画出决策树

--- test.py
+++ test.py
@@ -1,7 +1,17 @@
 import trees
+import treePlotter
 myData, labels = trees.createDataSet()
-print(trees.chooseBestFeatureToSplit(myData))
+#print(trees.chooseBestFeatureToSplit(myData))
 #print(trees.splitDataSet(myData, 0, 1))
 #print(trees.splitDataSet(myData, 0, 0))
-print(myData)
+#print(myData)
+#classList = [3, 4, 5, 2, 2, 4, 4]
+#print(trees.majorityCnt(classList))
 #print(trees.calcShannonEnt(myData))
+#myTree = trees.createTree(myData, labels)
+
+#treePlotter.createPlot()
+myTree = treePlotter.retrieveTree(0)
+myTree['no surfacing'][3] = 'maybe'
+print(myTree)
+treePlotter.createPlot(myTree)
